e1247485a4765068beeca18f6c0f1db3e2735478
pdelong <pgd22@njit.edu>
2018-02-07 19:17:50 -0500

    asd

--- middle/login.php
+++ middle/login.php
@@ -3,7 +3,7 @@
 #$url = 'https://www6.njit.edu/cp/login.php';
 $db = 'http://web.njit.edu/~sdp53/cs490/login.php';
 
-$data = array('sdp53', 's');
+$data = json_encode(array('Username' => 'sdp53', 'Password' => 'password'),true);
 // User name and password should be passed as an array
 #$data = json_decode(file_get_contents('php://input'), true));
 
@@ -14,7 +14,8 @@ $ch = curl_init($db);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-    $response = curl_exec ($ch);
-    echo $response;
+    $res = curl_exec($ch);
+    $response = json_decode($res, true);
+    echo $response["Response"];
 
 ?>
