dffbac63e1df332a2867c06405658a77fdfb3dbc
Bluetricity <kentaris@gmail.com>
2018-02-07 22:31:55 -0500

    Add files via upload

--- front/check.php
+++ front/check.php
@@ -1,21 +1,22 @@
 <?php
-  session_start();
+	session_start();
 	$Username=$_POST['name'];
 	$Password=$_POST['pass'];
-  $field=array('Username'=>$Username, 'Password'=>$Password);
-  $send=json_encode($field);
+  	$field=array('Username'=>$Username, 'Password'=>$Password);
+  	$send=json_encode($field);
   
-  $curl=curl_init();
+  	$curl=curl_init();
+  	
+	//temp url
+  	$url="https://web.njit.edu/~sdp53/cs490/login.php";
   
-  $url="https://web.njit.edu/~sdp53/cs490/login.php";
-  
-  curl_setopt_array($curl, array(
-    CURLOPT_URL => $url,
-    CURLOPT_POST => 1,
-    CURLOPT_FOLLOWLOCATION => 1,
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_POSTFIELDS => $send
-  ));
+  	curl_setopt_array($curl, array(
+    	 CURLOPT_URL => $url,
+    	 CURLOPT_POST => 1,
+    	 CURLOPT_FOLLOWLOCATION => 1,
+    	 CURLOPT_RETURNTRANSFER => 1,
+    	 CURLOPT_POSTFIELDS => $send
+  	));
 $resp = curl_exec($curl);
 $response=json_decode($resp,true);
 echo $resp;
