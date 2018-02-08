0c7f3c27283ef45997f1f12e7327d4b4ab0ea372
Bluetricity <kentaris@gmail.com>
2018-02-08 17:56:47 -0500

    Add files via upload

---
+++ front/login.php
@@ -0,0 +1,47 @@
+
+<head>
+        <h1>LOGIN</h1>
+</head>
+
+<body>
+<div class="signIn">
+
+<fieldset>
+	Username:<input id="user" name="user" type="text"/><br /><br/>
+	Password:<input id="pwd" name="pwd" type="password"/><br /><br />
+	<input name="btn" type="submit" value="login" onClick="aPost(this.fieldset);">
+</fieldset>  
+       
+<div id="status"></div>
+
+<p id="test">test</p>
+
+</body>
+
+<script language="javascript">
+function aPost(){
+    var hr = new XMLHttpRequest();
+    var url = "check.php";
+    var u = document.getElementById("user").value;
+    var p = document.getElementById("pwd").value;
+    var vars = "ucid="+u+"&pass="+p;
+      hr.open("POST", url , true);
+      hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	  
+      hr.onreadystatechange = function() {
+	      if(hr.readyState == 4 && hr.status == 200) {
+		      
+			  var return_data = hr.responseText;
+			  console.log(return_data);
+			  
+			  var myobj = JSON.parse(return_data);
+			  
+			  console.log(myobj);
+			  
+			  document.getElementById('test').innerHTML=("test");
+			   
+	    }
+    }
+      hr.send(vars); 
+	  }
+</script>
