b43181c3e72bc6d9498c93812f12558b0be3f24f
pdelong <pgd22@njit.edu>
2018-02-07 22:05:10 -0500

    current

--- middle/login.php
+++ middle/login.php
@@ -1,6 +1,7 @@
 <?php
 // This is where we want to post to
 #$url = 'https://www6.njit.edu/cp/login.php';
+$db = 'https://web.njit.edu/~sdp53/cs490/login.php';
 
 
 $data = array('Username' => "sdp53", 'Password' => "password");
@@ -8,16 +9,16 @@ $data_obj = json_encode($data, true);
 // User name and password should be passed as an array
 #$data = json_decode(file_get_contents('php://input'), true));
 
-/*
+
 $ch = curl_init($db);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_obj);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-*/
+
+/*
     $curl=curl_init();
-    $db = 'http://web.njit.edu/~sdp53/cs490/login.php';
 
     curl_setopt_array($curl,array(
         CURLOPT_URL => $db,
@@ -27,7 +28,8 @@ $ch = curl_init($db);
         CURLOPT_POSTFIELDS => $data_obj
 
     ));
-    $response = curl_exec($curl);
+*/
+    $response = curl_exec($ch);
     $array = json_decode($response, true);
     echo $array["Response"];
 
