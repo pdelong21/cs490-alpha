743525e4fd3ce11b9b658ca9ba2989b157e4e11c
pdelong <pgd22@njit.edu>
2018-02-07 23:24:15 -0500

    should be working

--- middle/login.php
+++ middle/login.php
@@ -1,37 +1,66 @@
 <?php
-// This is where we want to post to
-#$url = 'https://www6.njit.edu/cp/login.php';
-$db = 'https://web.njit.edu/~sdp53/cs490/login.php';
+/*
+    Author: Patrick Delong
+    Resources used: php.net/manual
+*/
+    // This is where we want to post to and get our response back from
+    $url = 'https://www6.njit.edu/cp/login.php';
+    $db = 'https://web.njit.edu/~sdp53/cs490/login.php';
 
+    # Accept the json data from the front & decode, we are passing an array
+    $data = file_get_contents(json_decode('php://input'));
+    #$data = array('Username' => 'sdp53', 'Password' => 'password');
 
-$data = array('Username' => "sdp53", 'Password' => "password");
-$data_obj = json_encode($data, true);
-// User name and password should be passed as an array
-#$data = json_decode(file_get_contents('php://input'), true));
+    # Pass the data to a json object
+    $data_obj = json_encode($data, true);
 
-
-$ch = curl_init($db);
+    # Create curl session $ set options
+    $ch = curl_init($db);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_obj);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-/*
-    $curl=curl_init();
-
-    curl_setopt_array($curl,array(
-        CURLOPT_URL => $db,
-        CURLOPT_POST => 1,
-        CURLOPT_FOLLOWLOCATION => 1,
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_POSTFIELDS => $data_obj
-
-    ));
-*/
-
+    # Execute curl request
     $response = curl_exec($ch);
-    $array = json_decode($response, true);
-    echo $array["Response"];
 
-?>
+    # Decode the response
+    $data_res = json_decode($response, true);
+
+    # Echo the json object back to the front if it was valid
+    if($data_res['Response'] == 'VALID'){
+        echo json_encode($data_res['Response'], true);
+        #print_r($data_res);
+    }
+    # Try to log into NJIT instead
+    else if($data_res['Response'] == 'INVALID'){
+        # Close previous session
+        curl_close($ch);
+
+        # Create new session
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_obj);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        $data_res = json_decode($response, true);
+
+        if(is_null($data_res)){
+            echo json_encode("Wrong password for NJIT \n");
+        }
+        else{
+            echo json_encode("You got in!\n");
+        }
+        #print_r($data_res);
+    }
+    else {
+        echo "Something didn't work right\n";
+    }
+
+    // Free up resources
+    curl_close($ch);
+
+
