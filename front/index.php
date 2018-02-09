11773507c69c760c86b8353e8a2ab4d3875291ec
Sunny Patel <sunny96137@gmail.com>
2018-02-08 19:13:56 -0500

    Update index.php

--- front/index.php
+++ front/index.php
@@ -1,95 +1,27 @@
-
-<!DOCTYPE html>
 <html>
 <head>
 	<h1>LOGIN</h1>
 </head>
 <body>
-	<!-- Login Form -->
-	<p>UCID:</p>
-	<input id="user" type="text" value=""></input>
-
-	<p>PASSWORD:</p>
-	<input id="pwd" type="password" value=""></input>
-
-	<p></p>
-
-	<button type="button" id="login" 
-	  onclick="userIn()">Log in</button>
-
-	<button type="button" id="reset" 
-	  onclick="clearFields()">Clear Fields</button>
-
-	<!-- Check and response -->
-	<p id="test"></p>
-	<p id="test2"></p>
-
-	<!-- scripts -->
-	<script>
-		function userIn(){
-			var req=new XMLHttpRequest();
-			var url="check.php";
-			var name=document.getElementById('user').value;
-			var pass=document.getElementById('pwd').value;
-			var cred=[name, pass];
-
-			//Start of AJAX part I think
-			req.open("POST", url, true);
-			req.onreadystatechange=function(){
-        		if(req.readyState==4 && req.status==200){
-				var rData=req.responseText;
-          			console.log(rData);
-          			var obj=JSON.parse(rData);
-          			console.log(obj);
-//				var test="<?php echo $response ?>";
-				document.getElementById('test').innerHTML=(rData);
-				document.getElementById('test2').innerHTML=(obj);
-         
-//				$.ajax({
-//				type	:	'POST',
-//				url	:	'check.php',
-//				data	:	{name, pass},
-//				success	:	fuction(){
-
-//				add if statements for:
-//				NJIT=Yes,Database=No
-//				NJIT=No,Database=Yes
-//				NJIT=No,Database=No
-//				then change the reponse like below to the correct output
-
-
-//				if(rData=="")
-//					document.getElementById('test').innerHTML=(cred);
-					document.getElementById('test2').innerHTML=(obj);
-//					document.getElementById('test').innerHTML=(test);
-          			}
-      			}
-      		req.send(cred);
-//			});
-      
-		}
-	</script>
-
-	<script>
-		function clearFields(){
-			document.getElementById('user').value="";
-			document.getElementById('pwd').value="";
-		  	document.getElementById('test').innerHTML="";
-		}
-	</script>
+	<form method="post">
+    Username:  <input type="text" name="username" /><br />
+    Password: <input type="password" name="password" /><br />
+    <input type="submit" name="submit" value="Submit" />
+  </form>
+ 
+ 
+ 
 </body>
 </html>
 
+
 <?php
-	session_start();
-	$Username=$_POST['name'];
-	$Password=$_POST['pass'];
+  session_start();
+  $Username=$_POST['username'];
+	$Password=$_POST['password'];
 #	$url'=https://web.njit.edu/~pgd22/middle/login.php';
   	$field=array('Username'=>$Username, 'Password'=>$Password);
-  	$send=json_encode($field);
-	echo $Username;
-	echo $Password;  	
-
+  	$send=json_encode($field); 	
   	$curl=curl_init();
   	
 #	temp url
