dbc381d49a05ce6e46abb046e5b87f921f1fc9f8
Sunny Patel <sunny96137@gmail.com>
2018-02-20 13:07:13 -0500

    Create Documentation for login.php

---
+++ back/Documentation for login.php
@@ -0,0 +1,20 @@
+Username and Passwords
+
+For Professor
+User: teach1
+Pass: passteach1
+
+For Student 1
+User: std1
+Pass: passstud1
+
+For Student 2
+User: std2
+Pass: passstud2
+
+For the login the database will either say the person is either Student or Professor. It will say INVALID if they are niether. 
+
+Examples:
+{"Response":"INVALID"}
+{"Response":"Student"}
+{"Response":"Professor"}
