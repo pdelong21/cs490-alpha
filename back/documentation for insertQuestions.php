0e868a4aee443d34ee90d16ebfcf2776366a8c42
Sunny Patel <sunny96137@gmail.com>
2018-02-20 14:02:01 -0500

    Create documentation for insertQuestions.php

---
+++ back/documentation for insertQuestions.php
@@ -0,0 +1,5 @@
+Takes 'Question' 'Category' 'Difficulty' 'Points' 'Cases' Everything is a string except points, points is an integer.
+
+Sample Output
+{"Response":"INVALID"}
+{"Response":"Successfully Inserted"}
