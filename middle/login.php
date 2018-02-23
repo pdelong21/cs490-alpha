9e30c367eacb18774af499e1d39552cbe17824db
pdelong <pgd22@njit.edu>
2018-02-22 22:09:05 -0500

    current

--- middle/login.php
+++ middle/login.php
@@ -1,69 +1,42 @@
 <?php
-/*
-    Author: Patrick Delong
-    Resources used: php.net/manual
-*/
-    // This is where we want to post to and get our response back from
-    $url = 'https://cp4.njit.edu/cp/home/login';
-    $db = 'https://web.njit.edu/~sdp53/cs490/login.php';
+/**
+ * Created by PhpStorm.
+ * User: pdelong
+ * Date: 2/22/18
+ * Time: 8:33 PM
+ */
+// Login Database
+$loginUrl= 'https://web.njit.edu/~sdp53/cs490/login.php';
 
-    # This array goes to front
-    $to_front = array();
-
-    # Accept the json data from the front & decode, we are passing an array
-    $getdata = file_get_contents('php://input');
-    $data = json_decode($getdata, true);
-
-    # Pass the data to a json object
-    $data_obj = json_encode($data, true);
-
-    # Create curl session to login to the database
-    $ch = curl_init($db);
-    curl_setopt($ch, CURLOPT_POST, 1);
+// Curl func
+function Login($data_obj, $url){
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_obj);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    # Execute curl request
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
-    $data_res = json_decode($response, true);
+    $r_decoded = json_decode($response, true);
     curl_close($ch);
+    return $r_decoded;
 
-    # Throw response into array
-    if($data_res['Response'] == 'VALID'){
-        $to_front += ['db' => 'VALID'];
-        #print_r($data_res);
-    }
-    # Try to log into NJIT instead
-    else if($data_res['Response'] == 'INVALID'){
-        $to_front += ['db' => 'INVALID'];
-    }
-    else {
-        // Should never get here
-        echo "Something didn't work right\n";
-    }
+}
 
-    # Create new session for logging into NJIT
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch,CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, 'user='.$data['Username'].'&pass='.$data['Password'].'&uuid=0xACA021');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+# Get input from front to forward
+$login_obj = file_get_contents('php//input');
+//$login_data = json_encode(['Username' => 'std2', 'Password' => 'passstud2'], true);
 
-    # Execute curl request
-    $response = curl_exec($ch);
-    curl_close($ch);
-    # throw response into array
-    if(strpos($response,"Failed Login") != FALSE){
-        $to_front += ['njit' => 'INVALID'];
-    }
-    else if(strpos($response, "Login Successful") != FALSE){
-        $to_front += ['njit' => 'VALID'];
-    }
+# Retrieve response from backend
+$loginType = Login($login_obj,$loginUrl);
+//$loginType = Login($login_data, $loginUrl);
 
-    # json array back to front
-    echo json_encode($to_front, true);
+$loginType_obj = json_encode($loginType, true);
+echo $loginType_obj;
 
-
+# Redirect to appropiate page
+if($loginType['Response'] == 'Student'){
+    header('Location: https://web.njit.edu/~jp484/490/student.php');
+}
+else{
+    #bleh
+}
