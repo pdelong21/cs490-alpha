9a3348c210b4d0579306406de62d8462031babb8
pdelong <pgd22@njit.edu>
2018-03-02 11:24:47 -0500

    working on grading

--- middle/grade.php
+++ middle/grade.php
@@ -5,3 +5,19 @@
  * Date: 3/2/18
  * Time: 10:41 AM
  */
+
+function compileMe($str_code){
+
+}
+
+function gradeMe($case, $std_ans){
+    $points = 0;
+    switch ($case){
+        case 0:
+            #check to see if it compiled or not
+
+    }
+}
+
+#this is the students input to test
+$std_res_obj = file_get_contents('php://input');
