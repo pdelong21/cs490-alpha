36bdd6a53070cab0d683c589f13ed3753e63cd7d
pdelong <pgd22@njit.edu>
2018-03-03 23:41:19 -0500

    added many functions for stripping spaces out of answers for format, then added in the ability to write over an existing file, compile it, and now will be able to grade once I have more input to go off of.

--- middle/grade.php
+++ middle/grade.php
@@ -9,7 +9,7 @@ $url = 0 /* sunnys database that will store the grades*/ ;
 $ans= array(
     'Id' => array(
         'User' => 'someonesmart',
-        'Answer' => 'a smart answer',
+        'Answer' => "x=10 \n y=12 \n z=x+y \n print(str(z))",
         'Cases' => 'f(2,2)',
         'Points' => 10
     )
@@ -28,6 +28,12 @@ function handIn($data_obj, $url){
 
 }
 
+function writeFile($python_file, $student_res){
+    $handle = fopen($python_file, 'w') or die("Can't open yo");
+    fwrite($handle, $student_res);
+    fclose($handle);
+}
+
 function compileMe($py_file){
     #$file ;
     $cmd = 'python ./'.$py_file;
@@ -43,6 +49,7 @@ function gradeMe($case, $std_ans){
     switch ($case){
         case 0:
             #check to see if it compiled or not -- 1 point
+
         case 1:
             #check to see if the function name matches -- 1 point
         case 2:
@@ -56,6 +63,6 @@ function gradeMe($case, $std_ans){
     array[user, questionID,
 */
 #$std_res_obj = file_get_contents('php://input');
-
-echo $return_status;
+writeFile('python.py',str_replace(' ', '', $ans['Id']['Answer']));
+$output = compileMe('python.py');
 print_r($output);
