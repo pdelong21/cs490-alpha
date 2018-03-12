f548474b2a2ef4e23e9df677b9446d01775243b8
Sunny Patel <sunny96137@gmail.com>
2018-03-11 20:35:32 -0400

    Update insertQuestions.php

--- back/insertQuestions.php
+++ back/insertQuestions.php
@@ -9,11 +9,11 @@ $response = file_get_contents('php://input');
 $decoder = json_decode($response,true); 
 
 $question=$decoder['Question'];
-$difficulty=$decoder['Difficulty'];
-$points =$decoder['Points'];      
-$cases =$decoder['Cases']; 
-
-//echo $question;
+$difficulty=$decoder['Difficulty'];    
+$cases =array();
+$cases= $decoder['Cases']; 
+$signature=$decoder['Signature'];
+// $question;
 
 // Create connection
 $conn = mysqli_connect($dbserver, $user, $password, $database);
@@ -23,18 +23,34 @@ if (!$conn) {
 }
 
 
-$sql1 = mysqli_query($conn,"SELECT max(Id) FROM Questions");
-			$row= mysqli_fetch_assoc($sql1);
-      $id=$row['max(Id)']+1;
-$sql2 = "INSERT INTO Questions (Id, Question, Difficulty, Points, TestCases) VALUES($id,'$question', '$difficulty', $points, '$cases')";
+$sql1 = "SELECT * FROM Questions";
+			
+			$result1 = $conn->query($sql1);
+$id=$result1->num_rows;
+$sql2 = "INSERT INTO Questions (Id, Question, Difficulty,Signature) VALUES($id,'$question', '$difficulty','$signature')";
 			
 			$result2 = $conn->query($sql2);
-			  if($result2){
+      $c=count($cases);
+      
+      for ($x = 0; $x <$c; $x++) {
+    
+      $sql3 = "SELECT * FROM TestCases";
+			
+			$result3 = $conn->query($sql3);
+$tId=$result3->num_rows;
+$caseT=$cases[$x][0];
+$caseA=$cases[$x][1];
+$sql4 = "INSERT INTO TestCases (Id, QuestionId, Testcase, Answer) VALUES($tId,$id,'$caseA','$caseT')";
+			
+			$result4 = $conn->query($sql4);
+      
+} 
+			  if($result4){
 				  $log = array("Response"=>"Successfully Inserted");
 				  echo json_encode($log,true);
         }
         else{
-          $log = array("Response"=>$sql2);
+          $log = array("Response"=>"Just Quit");
 				  echo json_encode($log,true);
         }
 mysqli_close($conn);
