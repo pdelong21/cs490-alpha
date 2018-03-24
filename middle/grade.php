f7f00a061bc23c50ab6765af891d09edd2f25d8b
pdelong <pgd22@njit.edu>
2018-03-24 10:09:34 -0400

    grading process is currently working however I do need to add the cases so this is just a basic version up and running

--- middle/grade.php
+++ middle/grade.php
@@ -8,6 +8,7 @@
 $handIn_url = 'https://web.njit.edu/~sdp53/cs490/insertGrade.php' /* sunnys database that will store the grades*/ ;
 $test_url = 'https://web.njit.edu/~sdp53/cs490/getTest.php';
 $cases_url = 'https://web.njit.edu/~sdp53/cs490/getTestCases.php';
+$ques_url = 'https://web.njit.edu/~sdp53/cs490/displayQuestions.php';
 $max_points = 0;
 $points_recieved_arr = array();
 
@@ -51,6 +52,18 @@ function getTest($url){
 
 }
 
+function getCases($data_obj, $url){
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_obj);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    $r_decoded = json_decode($response, true);
+    curl_close($ch);
+    return $r_decoded;
+}
+
 function writeFile($python_file, $student_res){
     $handle = fopen($python_file, 'w') or die("Can't open...");
     fwrite($handle, $student_res);
@@ -78,20 +91,25 @@ function gradeMe($case, $std_ans, $func_case){
             #check to see if it compiled or not -- 1 point or zero for whole problem
             if(end($output) == 0 && $std_ans != null){
                 $points ++;
-                $feedback = $feedback.'Your program compiled! ';
-            } else return $points;
+                $feedback = $feedback."Your program compiled! ";
+            } else{
+                $feedback = $feedback."Your program failed to compile so I must end it here.";
+                continue;
+            }
         case 1:
             #check to see if the function name matches -- 1 point
             if (strpos($std_ans, $func_case) == FALSE) {
+                $feedback = $feedback."You must have learned to read at the Derek Zoolander center for kids who can't read good 
+                because the function name is wrong.";
                 continue;
             } else{
                 $points+=1;
-                $feedback = $feedback.'The function name matches. ';
+                $feedback = $feedback."The function name matches, crowd goes wild! ";
             }
         case 2:
             #check to see if output is correct or not -- 3 points -- check in progress
             $points+=3;
-            $feedback = $feedback."And the rest is tbc so for now you got test case 3... so proud :')";
+            $feedback = $feedback."And the rest is tbc so for now you got test case 3... so proud.";
     }
     $points = $points/5.0;
     $array = ['Points' => $points, 'Feedback' => $feedback];
@@ -124,35 +142,27 @@ function percentGrade($points_array, $maxpoints){
 $ans_obj = file_get_contents('php://input');
 $ans_decoded = json_decode($ans_obj, true);
 
-#$ans_decoded [] = "pgd22";
-
 $username = $ans_decoded['User'];
 $std_test ['Username'] = $username;
 $std_test ['Question'] = array();
 
-#$ans_decoded[]= "def func():\n\tx=1";
-#$ans_decoded[] = "asdasd";
 
 # Retrieve the exam for grading
 
 $test_obj = getTest($test_url); # contains an array of arrays - format [nth Ques] -> Ass. Array()
-//print_r($test_obj);
+#print_r($test_obj);
 #echo $test_obj[0]['Points'];
 # Start grading process -- returns double
 for ($i=0; $i < count($ans_decoded['Answers']); $i++){
     $max_points += $test_obj[$i]['Points']; # points possible on test
-    //preg_match('/"([a-zA-Z]+)"/', $test_obj[$i-1]['Question'], $m); #get func name from question
-    //$func_name = end($m);
-    #preg_match_all('/([0-9]+)\s|[0-9]$/',$test_obj[$i-1]['TestCases'],$c);
-    #print_r($c);
     $grade_res = gradeMe(0, $ans_decoded['Answers'][$i],$test_obj[$i]['Signature']); # returns array of points and feedback
     $points_recieved_arr [] = $grade_res['Points']*$test_obj[$i]['Points']; # tally of points recieved
-    $std_test ['Question'][$i] = array('Response' => $ans_decoded['Answers'][$i], 'Points' => $points_recieved_arr[$i],
-        'Feedback' => $grade_res['Feedback']);
+    $std_test ['Question'][$i] = array('QuestionId' => $test_obj[$i]['QuestionId'],'Response' => $ans_decoded['Answers'][$i], 'Points' => $points_recieved_arr[$i],
+        'MaxPoints' => $test_obj[$i]['Points'], 'Feedback' => $grade_res['Feedback']);
 }
 
 # get the percentage of the test grade
 $std_test ['Grade'] = percentGrade($points_recieved_arr, $max_points);
-$echo_back_json = json_encode($std_test);
-#$var = handIn($echo_back_json, $handIn_url);
-echo json_encode($std_test, true);
+$handIn_obj = json_encode($std_test);
+$handIn_res = handIn($handIn_obj, $handIn_url);
+echo json_encode($handIn_res, true);
